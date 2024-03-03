bb46dc8be998222fa7511783e133f7781ae322bf
KrishnaMohan1305 <159781697+KrishnaMohan1305@users.noreply.github.com>
2024-03-03 20:34:21 +0530

    from streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,3 +58,7 @@ streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
+
+
+streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit');")
